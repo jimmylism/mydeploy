bade94fa46b3e7eaa8ce7c5c7c45a84d50fb5caa
jimmylism <32603103+jimmylism@users.noreply.github.com>
2019-10-21 20:21:52 -0400

    1

--- src/views/LoginPage/LoginForm.js
+++ src/views/LoginPage/LoginForm.js
@@ -71,7 +71,7 @@ export default class LoginForm extends React.Component {
       })
       .catch(error => {
         console.log(error)
-        this.setState({error: error, response1: error})
+        return error;
       })
   };
 
